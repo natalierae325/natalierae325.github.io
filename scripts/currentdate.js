6626b54a4b59d54df860c24b1d6fdf58c37c6d7b
Natalie Gibson <35414098+natalierae325@users.noreply.github.com>
2018-02-26 17:13:38 -0700

    responisve images

---
+++ scripts/currentdate.js
@@ -0,0 +1,8 @@
+
+<script>
+        var currentDate = new Date(),
+        day = currentDate.getDate(),
+        month = currentDate.getMonth() + 1,
+        year = currentDate.getFullYear();
+        document.write(day + "/" + month + "/" + year)
+    </script>
