80da1c5c283fd7afd566217df05043ce018d826a
Natalie Gibson <35414098+natalierae325@users.noreply.github.com>
2018-02-27 16:10:47 -0700

    gallery

--- scripts/currentdate.js
+++ scripts/currentdate.js
@@ -1,8 +1,8 @@
-
-<script>
+function currentdate(){
+  
         var currentDate = new Date(),
         day = currentDate.getDate(),
         month = currentDate.getMonth() + 1,
         year = currentDate.getFullYear();
-        document.write(day + "/" + month + "/" + year)
-    </script>
+        document.write(day + "/" + month + "/" + year);
+}
