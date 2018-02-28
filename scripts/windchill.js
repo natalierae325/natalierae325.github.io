ea0e3295eec71dd7e99dd55ae09d74363dfc2ecf
Natalie Gibson <35414098+natalierae325@users.noreply.github.com>
2018-02-27 23:56:32 -0700

    gallery and responsive

---
+++ scripts/windchill.js
@@ -0,0 +1,6 @@
+function windchill(temp, speed) {
+var temp = t;
+var speed = s;
+var fahrenheit = 35.74 + 0.6215 * t - 35.75 (math.pow(s, 0.16)) + 0.4275 * t (math.pow(s, 0.16));
+return windchill;
+}
